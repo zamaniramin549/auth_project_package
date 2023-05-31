f785546b274627247ce09a9dbfe67c29781ae35e
Ramin Zamani <ramin@lizard.global>
2023-05-31 13:05:41 +0800

    new verssion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'auth_package',         # How you named your package folder (MyLib)
   packages = ['auth_package'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.1',      # Start with a small number and increase it with every change you make
   license='mit',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Auth package for authentication with microservices',   # Give a short description about your library
   author = 'Ramin Zamanighiri',                   # Type in your name
